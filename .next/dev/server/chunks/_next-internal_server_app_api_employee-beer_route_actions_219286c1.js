0019d80cab1fc508f715e8a1d85980d5706b6df0
des arvidsson <desarvidsson@gmail.com>
2025-12-17 23:21:37 -0800

    chore: ignore .next build output

--- .next/dev/server/chunks/_next-internal_server_app_api_employee-beer_route_actions_219286c1.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/employee-beer/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_employee-beer_route_actions_219286c1.js.map
